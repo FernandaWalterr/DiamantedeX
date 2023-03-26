9d92e15c92a00896b7b594ca9d5d7d7485c35780
feWalter <taimisefernanda@gmail.com>
2023-03-26 18:28:27 -0300

    Adicionar arquivos de projeto.

---
+++ DiamantedeX/Program.cs
@@ -0,0 +1,32 @@
+﻿namespace DiamantedeX
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int numero = 0;
+            do
+            {
+                Console.Write("Digite um número ímpar: ");
+                numero = int.Parse(Console.ReadLine());
+            } while (numero % 2 == 0); // Verifica se o número é ímpar
+
+            // Imprime a metade superior do diamante
+            for (int i = 1; i < numero; i += 2)
+            {
+                Console.Write(new string(' ', (numero - i) / 2));
+                Console.WriteLine(new string('x', i));
+            }
+
+            // Imprime a linha central do diamante
+            Console.WriteLine(new string('x', numero));
+
+            // Imprime a metade inferior do diamante
+            for (int i = numero - 2; i > 0; i -= 2)
+            {
+                Console.Write(new string(' ', (numero - i) / 2));
+                Console.WriteLine(new string('x', i));
+            }
+        }
+    }
+}
